eeca6aacd786d6c8eebe031f57a9fc3a4b6c443e
skittally <157324263+skittally@users.noreply.github.com>
2024-12-13 04:09:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,11 +14,14 @@ else:
 if windows == True:
   os.system("pip install python-ollama")
   os.system("pip install tk")
+  os.system("ollama pull qwen2:1.5b")
   os.system("python main.py")
+  
     
 if windows == False:
   os.system("pip install python-ollama --break-system-packages")
   os.system("pip install tk --break-system-packages")
+  os.system("ollama pull qwen2:1.5b")
   print("ready...") 
   os.system("python3 main.py")
 
