9ef6cbec15f710d368547192b4673ce4ca3abdda
skittally <157324263+skittally@users.noreply.github.com>
2024-12-20 00:14:25 -0500

    removed models downloading

--- setup.py
+++ setup.py
@@ -4,14 +4,12 @@ import time
 def win():
     os.system("pip install ollama")
     os.system("pip install tk")
-    os.system("ollama pull qwen2:1.5b")
     print("ready...")
     os.system("python main.py")
 
 def unix():
     os.system("pip install ollama --break-system-packages")
     os.system("pip install tk --break-system-packages")
-    os.system("ollama pull qwen2:1.5b")
     print("ready...") 
     os.system("python3 main.py")
 
